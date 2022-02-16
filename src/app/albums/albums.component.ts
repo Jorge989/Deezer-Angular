2e1ede13a844fb376500ad01979b952166ba2509
Jorge <jorge.attie@br.ey.com>
2022-02-16 10:02:18 -0300

    musicas

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+import { HostListener } from "@angular/core";
+@HostListener('window:scroll', ['$event'])
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
@@ -10,8 +12,12 @@ export class AlbumsComponent implements OnInit {
   audio = new Audio();
   faPlay = faPlay;
   faPause = faPause;
-  marilha = true;
+  marilha = false;
   id = 0;
+  counter:any =0;
+  teste=false;
+  control1 =window.pageYOffset;
+  control2 =document.querySelector('.container') as HTMLElement;
   constructor() {}
 
   ngOnInit(): void {}
@@ -56,4 +62,145 @@ export class AlbumsComponent implements OnInit {
       audio2.pause();
     }
   }
-}
+
+  playSound3(id:number) {
+    this.id = id;
+    let audio1 = this.audio;
+    audio1.src = '../assets/gustavo-lima.mp3';
+    audio1.load();
+    this.player = true;
+    if (this.player == true) {
+      audio1.play();
+    }
+  }
+  stopSound3(id:number) {
+    this.id = id;
+    let audio2 = this.audio;
+    audio2.src = '../assets/gustavo-lima.mp3';
+    audio2.load();
+    this.player = false;
+    if (this.player == false) {
+      audio2.pause();
+    }
+  }
+  playSound4(id:number) {
+    this.id = id;
+    let audio1 = this.audio;
+    audio1.src = '../assets/joao-gomes.mp3';
+    audio1.load();
+    this.player = true;
+    if (this.player == true) {
+      audio1.play();
+    }
+  }
+  stopSound4(id:number) {
+    this.id = id;
+    let audio2 = this.audio;
+    audio2.src = '../assets/joao-gomes.mp3';
+    audio2.load();
+    this.player = false;
+    if (this.player == false) {
+      audio2.pause();
+    }
+  }
+  playSound5(id:number) {
+    this.id = id;
+    let audio1 = this.audio;
+    audio1.src = '../assets/ze-felipe.mp3';
+    audio1.load();
+    this.player = true;
+    if (this.player == true) {
+      audio1.play();
+    }
+  }
+  stopSound5(id:number) {
+    this.id = id;
+    let audio2 = this.audio;
+    audio2.src = '../assets/ze-felipe.mp3';
+    audio2.load();
+    this.player = false;
+    if (this.player == false) {
+      audio2.pause();
+    }
+  }
+  playSound6(id:number) {
+    this.id = id;
+    let audio1 = this.audio;
+    audio1.src = '../assets/gloria-groove.mp3';
+    audio1.load();
+    this.player = true;
+    if (this.player == true) {
+      audio1.play();
+    }
+  }
+  stopSound6(id:number) {
+    this.id = id;
+    let audio2 = this.audio;
+    audio2.src = '../assets/gloria-groove.mp3';
+    audio2.load();
+    this.player = false;
+    if (this.player == false) {
+      audio2.pause();
+    }
+  }
+  playSound7(id:number) {
+    this.id = id;
+    let audio1 = this.audio;
+    audio1.src = '../assets/renan.mp3';
+    audio1.load();
+    this.player = true;
+    if (this.player == true) {
+      audio1.play();
+    }
+  }
+  stopSound7(id:number) {
+    this.id = id;
+    let audio2 = this.audio;
+    audio2.src = '../assets/renan.mp3';
+    audio2.load();
+    this.player = false;
+    if (this.player == false) {
+      audio2.pause();
+    }
+  }
+  playSound8(id:number) {
+    this.id = id;
+    let audio1 = this.audio;
+    audio1.src = '../assets/ludmila.mp3';
+    audio1.load();
+    this.player = true;
+    if (this.player == true) {
+      audio1.play();
+    }
+  }
+  stopSound8(id:number) {
+    this.id = id;
+    let audio2 = this.audio;
+    audio2.src = '../assets/ludmila.mp3';
+    audio2.load();
+    this.player = false;
+    if (this.player == false) {
+      audio2.pause();
+    }
+  }
+  playSound9(id:number) {
+    this.id = id;
+    let audio1 = this.audio;
+    audio1.src = '../assets/dilsinho.mp3';
+    audio1.load();
+    this.player = true;
+    if (this.player == true) {
+      audio1.play();
+    }
+  }
+  stopSound9(id:number) {
+    this.id = id;
+    let audio2 = this.audio;
+    audio2.src = '../assets/dilsinho.mp3';
+    audio2.load();
+    this.player = false;
+    if (this.player == false) {
+      audio2.pause();
+    }
+  }
+  }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,21 @@ import { faBars} from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent implements OnInit {
   faHamburger = faBars;
+  control = false
+  active= ""
   constructor() { }
 
   ngOnInit(): void {
   }
+  handleMenu(){
 
+if(this.control ==  false){
+  this.active = "active";
+  this.control = true;
+}else if(this.control = true){
+  this.active = "";
+  this.control = false;
 }
+  }
+}
+
